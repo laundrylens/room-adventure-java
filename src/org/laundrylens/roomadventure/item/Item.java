767a244cb7bff593cdc7f2329b1093a716328498
Kedaruii <keda@tanuki.codes>
2025-05-20 16:42:24 -0500

    feat: add basic Item class implementation

---
+++ src/org/laundrylens/roomadventure/item/Item.java
@@ -0,0 +1,32 @@
+package item;
+
+public class Item {
+    private String name;
+    private String description;
+    private boolean grabbable;
+
+    public Item(String name, String description, boolean grabbable) {
+        this.name = name;
+        this.description = description;
+        this.grabbable = grabbable;
+    }
+
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.grabbable = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isGrabbable() {
+        return grabbable;
+    }
+
+}
